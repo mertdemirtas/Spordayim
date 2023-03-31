8804cf4f886bee0bd74b750ef502948700a75ab9
mertdemirtas <m.demirtas99@icloud.com>
2023-03-31 16:06:53 +0300

    Initial Commit

---
+++ Spordayim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spordayim
+//
+//  Created by Mert Demirtas on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
